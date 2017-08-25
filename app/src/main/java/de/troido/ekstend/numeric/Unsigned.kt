501755b58085cc95d667ddfeb9baf6492df2791b
Ivan Tokić <toxxmeister@gmail.com>
2017-08-25 18:18:29 +0200

    UShort and UInt support for Byte, and UInt support for Short

--- app/src/main/java/de/troido/ekstend/numeric/Unsigned.kt
+++ app/src/main/java/de/troido/ekstend/numeric/Unsigned.kt
@@ -6,7 +6,16 @@ import kotlin.experimental.and
 inline fun Byte.toUByte(): Short = toShort() and 0xff
 
 @Suppress("NOTHING_TO_INLINE")
+inline fun Byte.toUShort(): Int = toInt() and 0xff
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Byte.toUInt(): Long = toLong() and 0xff
+
+@Suppress("NOTHING_TO_INLINE")
 inline fun Short.toUShort(): Int = toInt() and 0xffff
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Short.toUInt(): Long = toLong() and 0xffff
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun Int.toUInt(): Long = toLong() and 0xffffffff
