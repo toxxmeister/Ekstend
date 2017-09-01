d1e9fb766bd22594159f2342a52d8eb4e6f5bece
Ivan Tokić <toxxmeister@gmail.com>
2017-09-01 17:12:39 +0200

    Tuple5 implementation

--- app/src/main/java/de/troido/ekstend/functional/Tuples.kt
+++ app/src/main/java/de/troido/ekstend/functional/Tuples.kt
@@ -8,3 +8,6 @@ fun <A, B, C, D> Pair<Pair<Pair<A, B>, C>, D>.flat(): Tuple4<A, B, C, D> =
 
 data class Tuple4<out A, out B, out C, out D>(val first: A, val second: B,
                                               val third: C, val fourth: D)
+
+data class Tuple5<out A, out B, out C, out D, out E>(val first: A, val second: B, val third: C,
+                                                     val fourth: D, val fifth: E)
