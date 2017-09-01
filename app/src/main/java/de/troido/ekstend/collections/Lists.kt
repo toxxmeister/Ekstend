2cca8b07b0cd1b5828fa90cbbe90487baca2baf3
Ivan Tokić <toxxmeister@gmail.com>
2017-09-01 17:33:51 +0200

    List components 6-8

--- app/src/main/java/de/troido/ekstend/collections/Lists.kt
+++ app/src/main/java/de/troido/ekstend/collections/Lists.kt
@@ -6,3 +6,15 @@ package de.troido.ekstend.collections
  */
 inline fun <T, reified U : T> List<T>.firstInstance(): U? =
         firstOrNull { it is U } as U
+
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <T> List<T>.component6(): T =
+        get(5)
+
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <T> List<T>.component7(): T =
+        get(6)
+
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <T> List<T>.component8(): T =
+        get(7)
