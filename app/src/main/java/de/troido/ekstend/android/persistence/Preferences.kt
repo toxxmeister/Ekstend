1f4242500e4b2d458927e0b0e90467f77c8e6352
Ivan Tokić <toxxmeister@gmail.com>
2017-09-07 16:40:52 +0200

    Aesthetics

--- app/src/main/java/de/troido/ekstend/android/persistence/Preferences.kt
+++ app/src/main/java/de/troido/ekstend/android/persistence/Preferences.kt
@@ -4,35 +4,20 @@ import android.content.SharedPreferences
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun SharedPreferences.getStringFloat(key: String, defValue: Float = 0.0f): Float =
-        getString(
-                key,
-                if (defValue == 0.0f) "0.0f" else defValue.toString()
-        ).toFloat()
+        getString(key, if (defValue == 0.0f) "0.0f" else defValue.toString()).toFloat()
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun SharedPreferences.getStringLong(key: String, defValue: Long = 0): Long =
-        getString(
-                key,
-                if (defValue == 0.toLong()) "0" else defValue.toString()
-        ).toLong()
+        getString(key, if (defValue == 0.toLong()) "0" else defValue.toString()).toLong()
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun SharedPreferences.getStringInt(key: String, defValue: Int = 0): Int =
-        getString(
-                key,
-                if (defValue == 0) "0" else defValue.toString()
-        ).toInt()
+        getString(key, if (defValue == 0) "0" else defValue.toString()).toInt()
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun SharedPreferences.getStringShort(key: String, defValue: Short = 0): Short =
-        getString(
-                key,
-                if (defValue == 0.toShort()) "0" else defValue.toString()
-        ).toShort()
+        getString(key, if (defValue == 0.toShort()) "0" else defValue.toString()).toShort()
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun SharedPreferences.getStringByte(key: String, defValue: Byte = 0): Byte =
-        getString(
-                key,
-                if (defValue == 0.toByte()) "0" else defValue.toString()
-        ).toByte()
+        getString(key, if (defValue == 0.toByte()) "0" else defValue.toString()).toByte()
